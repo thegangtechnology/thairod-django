48482af5b204d6beee29f7b418b7fd874f5e18d3
Atidhaya Yoochanachon <atidhaya.gift@gmail.com>
2021-07-27 11:27:31 +0700

    set nullable=true for shipment in orderitem

--- order/models/order_item.py
+++ order/models/order_item.py
@@ -5,7 +5,7 @@ from shipment.models import Shipment
 
 
 class OrderItem(AbstractModel):
-    shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE)
+    shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE, null=True)
     product_variation = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=3)
