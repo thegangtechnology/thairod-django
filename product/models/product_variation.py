7915bbca58d31090874dabb5f3c304a5a5162024
Atidhaya Yoochanachon <atidhaya.gift@gmail.com>
2021-07-27 16:21:10 +0700

    fix decimal places, add unit for productvariation

--- product/models/product_variation.py
+++ product/models/product_variation.py
@@ -1,6 +1,13 @@
 from django.db import models
 from core.models import AbstractModel
 from product.models import Product
+from django.utils.translation import gettext_lazy as _
+
+
+class ProductVariationUnit(models.TextChoices):
+    PIECES = 'PIECES', _('Pieces (เม็ด)')
+    PACKS = 'PACKS', _('Packs (แผง)')
+    BOXES = 'BOXES', _('Boxes (กล่อง)')
 
 
 class ProductVariation(AbstractModel):
@@ -8,3 +15,6 @@ class ProductVariation(AbstractModel):
     price = models.DecimalField(max_digits=10, decimal_places=3)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    unit = models.CharField(max_length=6,
+                            choices=ProductVariationUnit.choices,
+                            default=ProductVariationUnit.PIECES)
--- shipment/models/tracking_status.py
+++ shipment/models/tracking_status.py
@@ -5,7 +5,7 @@ from core.models import AbstractModel
 class TrackingStatus(AbstractModel):
     tracking_code = models.CharField(max_length=20)
     status = models.CharField(max_length=100)
-    price = models.DecimalField(decimal_places=0.3, max_digits=8)
-    discount = models.DecimalField(decimal_places=0.3, max_digits=8)
+    price = models.DecimalField(decimal_places=3, max_digits=8)
+    discount = models.DecimalField(decimal_places=3, max_digits=8)
     courier_code = models.CharField(max_length=10)
     courier_tracking_code = models.CharField(max_length=20)
