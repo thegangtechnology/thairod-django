3446221ffa9e673e15fb806e0f9ddd46763399d4
Ronnakrit Kunaviriyasiri <k.ronnakrit@thegang.tech>
2021-07-29 16:02:34 +0700

    Add dockerfile & github workflow

--- thairod/settings.py
+++ thairod/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 SHIPPOP_API_KEY = os.environ.get('SHIPPOP_API_KEY', "")
 SHIPPOP_URL = os.environ.get('SHIPPOP_URL', "https://mkpservice.shippop.dev")
