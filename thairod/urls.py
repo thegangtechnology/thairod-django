a699873b05316c7eda28e63987c8c4f638d118f9
Ronnakrit Kunaviriyasiri <k.ronnakrit@thegang.tech>
2021-07-29 16:17:08 +0700

    add pycodestyle rule

--- thairod/urls.py
+++ thairod/urls.py
@@ -20,18 +20,17 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Thairod Mall API",
-      default_version='v1',
-      description="",
-      terms_of_service="",
-      contact=openapi.Contact(email="k.ronnakrit@thegang.tech"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Thairod Mall API",
+        default_version='v1',
+        description="",
+        terms_of_service="",
+        contact=openapi.Contact(email="k.ronnakrit@thegang.tech"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -47,7 +46,7 @@ urlpatterns = [
     re_path(r'^docs/open_api(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^docs/open_api/$', schema_view.with_ui('swagger',
-            cache_timeout=0), name='schema-swagger-ui'),
+                                                     cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^docs/redoc/$', schema_view.with_ui('redoc',
-            cache_timeout=0), name='schema-redoc'),
+                                                  cache_timeout=0), name='schema-redoc'),
 ]
