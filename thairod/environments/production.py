d601ff3300e1c9a9fa7971a3dd965d19b3d68bfe
Ronnakrit Kunaviriyasiri <k.ronnakrit@thegang.tech>
2021-07-30 22:29:00 +0700

    Upgrade production

--- thairod/environments/production.py
+++ thairod/environments/production.py
@@ -1,10 +1,9 @@
 from ..settings import *
 
-ALLOWED_HOSTS = ['mall.thairod.care', '202.151.188.165']
+ALLOWED_HOSTS = ['server.mall.thairod.care', '202.151.188.165']
 
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    'https://server.mall.thairod.care',
-    'http://server.mall.thairod.care',
+    'https://mall.thairod.care',
 ]
