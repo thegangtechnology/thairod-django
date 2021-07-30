b784325d0ba80756ae8873888ed42260645e2fee
Piti <piti118@gmail.com>
2021-07-31 00:20:09 +0700

    add debug mode auth

---
+++ thairod/auth_debug.py
@@ -0,0 +1,20 @@
+from rest_framework import authentication
+
+from thairod import settings
+
+
+class DebugAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        if not settings.DEBUG:
+            return None
+        from user.models import User
+        user = User.objects.first()
+
+        if user is None:
+            password = User.objects.make_random_password()
+            user = User.objects.create(username='forceauth',
+                                       email='testpassuser@thegang.tech',
+                                       password=password,
+                                       is_staff=True, is_superuser=True
+                                       )
+        return (user, None)
