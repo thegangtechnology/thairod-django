7746d54245ab656547ad8cb1102e18190e3b70ff
Ronnakrit Kunaviriyasiri <k.ronnakrit@thegang.tech>
2021-07-30 21:51:06 +0700

    Add allowed_host

--- thairod/environments/production.py
+++ thairod/environments/production.py
@@ -5,5 +5,6 @@ ALLOWED_HOSTS = ['mall.thairod.care', '202.151.188.165']
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    'https://mall.thairod.care'
+    'https://server.mall.thairod.care',
+    'http://server.mall.thairod.care',
 ]
