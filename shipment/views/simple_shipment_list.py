334bfd1910745a8f509d0d3ad7bea43aaf63630c
Piti <piti118@gmail.com>
2021-07-31 04:35:26 +0700

    use basic auth

--- shipment/views/simple_shipment_list.py
+++ shipment/views/simple_shipment_list.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render
-from rest_framework import authentication
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
 from shipment.models import Shipment
-from user.models import User
-
-
-class TempAuth(authentication.BasicAuthentication):
-    def authenticate_credentials(self, userid, password, request=None):
-        if userid == 'hello' and password == 'world':
-            return User.objects.first(), None
-        else:
-            raise AuthenticationFailed('bad bad')
 
 
 class SimpleShipmentListView(APIView):
-    authentication_classes = [TempAuth]
+    authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request: Request):
