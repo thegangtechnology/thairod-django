8f85b9c69f7235f7e5ef2eb7b0662074fcb8122a
Piti <piti118@gmail.com>
2021-07-31 16:20:21 +0700

    add tracking link

--- shipment/templatetags/simple.py
+++ shipment/templatetags/simple.py
@@ -1,6 +1,8 @@
 from django import template
 from django.urls import reverse
 
+from thairod.services.shippop.api import ShippopAPI
+
 register = template.Library()
 
 
@@ -8,3 +10,8 @@ register = template.Library()
 def label_link(value):
     url = reverse('print-label')
     return f'{url}?shipments={value}'
+
+
+@register.filter(name='shippop_tracking')
+def shippop_tracking(value):
+    return ShippopAPI().tracking_link(value)
