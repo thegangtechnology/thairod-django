fd1af910b601a5f377e5b0b0c3011c0e457b7a72
Piti <piti118@gmail.com>
2021-07-31 12:03:41 +0700

    forbidden

--- thairod/utils/decorators.py
+++ thairod/utils/decorators.py
@@ -1,9 +1,12 @@
 import functools
+import logging
 from typing import List
 
 from django.http import HttpResponseForbidden
 
 from thairod.utils.html_tools import get_client_ip
+
+logger = logging.getLogger(__name__)
 
 
 def swagger_example(example):
@@ -31,7 +34,9 @@ def ip_whitelist(ip_list: List[str]):
             if ip in ip_list:
                 return f(self, request, *arg, **kwds)
             else:
-                raise HttpResponseForbidden(f'Access Denied for {ip}')
+                msg = f'Access Denied for {ip}'
+                logger.warning(msg)
+                return HttpResponseForbidden(msg)
 
         return ret
 
