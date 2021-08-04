55dcdc0a5f370c8b1f52d24f207681aa208f3d18
@Muenchan <mesxermorrakoth@gmail.com>
2021-08-04 15:18:09 +0700

    add current user api

--- user/tests.py
+++ user/tests.py
@@ -1,4 +1,6 @@
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from core.tests import BaseTestSimpleApiMixin
 from thairod.utils.test_util import APITestCase
@@ -20,3 +22,15 @@ class UserAPITestCase(APITestCase, BaseTestSimpleApiMixin):
             "first_name": "joe",
             "last_name": "don"
         }
+
+    def test_get_current_user(self):
+        response = self.client.get(reverse("current-user"))
+        self.assertEqual(response.data['username'], 'forceauth')
+        self.assertEqual(response.data['first_name'], 'joe')
+        self.assertEqual(response.data['last_name'], 'don')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_anonymous(self):
+        self.client.force_authenticate()
+        response = self.client.get(reverse("current-user"))
+        self.assertEqual(response.data['error'], 'user is anonymous.')
--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, views
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from user.models import User
-from user.serializers import UserSerializers
+from user.serializers import UserSerializers, CurrentUserSerializer
 
 
 class UserModelViewSet(viewsets.ModelViewSet):
@@ -11,3 +12,11 @@ class UserModelViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     filter_backends = [filters.SearchFilter]
     search_fields = ['username', 'email', 'first_name', 'last_name']
+
+
+class CurrentUserAPIView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_anonymous:
+            return Response({"error": "user is anonymous."})
+        return Response(CurrentUserSerializer(user).data)
