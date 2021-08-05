51af75f6a0137919b4e08a5523085026ef223e51
Ronnakrit Kunaviriyasiri <k.ronnakrit@thegang.tech>
2021-08-05 23:45:55 +0700

    Fix cor origin

--- thairod/environments/develop.py
+++ thairod/environments/develop.py
@@ -4,6 +4,6 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://mall.dev.thairod.care/",
-    "https://mall.dev.thairod.care/",
+    "http://mall.dev.thairod.care",
+    "https://mall.dev.thairod.care",
 ]
