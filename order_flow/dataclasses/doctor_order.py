fc874e50ab87f16ce02a346b07d6d69a0b6fad4e
Atidhaya Yoochanachon <atidhaya.gift@gmail.com>
2021-08-06 10:00:51 +0700

    remove dupliated code

--- order_flow/dataclasses/doctor_order.py
+++ order_flow/dataclasses/doctor_order.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from thairod.utils.auto_serialize import AutoSerialize
-from product.models.product_variation import ProductVariationUnit, ProductVariation
+from order_flow.dataclasses.ordered_product import OrderedProductInfo
 from order.dataclasses.cart_item import CartItem
 from typing import List
 
@@ -13,39 +13,6 @@ class DoctorOrder(AutoSerialize):
     def example(cls):
         return cls(
             items=[CartItem.example()])
-
-
-@dataclass
-class OrderedProductInfo(AutoSerialize):
-    id: int
-    name: str
-    price: int
-    description: str  # product variation description
-    product_description: str  # product description
-    unit: str
-    quantity: int
-
-    @classmethod
-    def from_cart_item(cls, cart_item: CartItem):
-        product_variation = ProductVariation.objects.get(pk=cart_item.item_id)
-        return cls(id=product_variation.id,
-                   name=product_variation.name,
-                   price=product_variation.price,
-                   description=product_variation.description,
-                   product_description=product_variation.product.description,
-                   unit=product_variation.unit,
-                   quantity=cart_item.quantity
-                   )
-
-    @classmethod
-    def example(cls):
-        return cls(id=1,
-                   name='product variation 1',
-                   price=10,
-                   description='product variation 1 description',
-                   product_description='product description',
-                   unit=ProductVariationUnit.PIECES.value,
-                   quantity=5)
 
 
 @dataclass
