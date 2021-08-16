bee69112e3c63661ae383d4c28f62644ac138277
Atidhaya Yoochanachon <atidhaya.gift@gmail.com>
2021-08-16 12:29:48 +0700

    read only for warehouse

--- warehouse/views.py
+++ warehouse/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets, filters
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from warehouse.models import Warehouse
 from warehouse.serializers import WarehouseSerializer
 
 
 class WarehouseModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = WarehouseSerializer
     queryset = Warehouse.objects.all()
     filter_backends = [filters.SearchFilter]
